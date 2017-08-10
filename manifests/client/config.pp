76b382d9821704dde20044beafa5c4d00523ce0b
Daniel Henninger <daniel@ncsu.edu>
2017-08-10 15:11:01 -0400

    Fix known_hosts hard coded path.

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -21,7 +21,7 @@ class ssh::client::config
   # Workaround for https://tickets.puppetlabs.com/browse/PUP-1177.
   # Fixed in Puppet 3.7.0
   if versioncmp($::puppetversion, '3.7.0') < 0 {
-    ensure_resource('file', '/etc/ssh/ssh_known_hosts', {
+    ensure_resource('file', $ssh::ssh_known_hosts, {
       'ensure' => 'file',
       'owner'  => $ssh::params::fileowner,
       'group'  => $ssh::params::filegroup,
